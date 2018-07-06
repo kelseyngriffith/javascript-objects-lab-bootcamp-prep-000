16d536755e24775a946384956d816782824fb624
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-07-06 01:41:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var recipes = {
   prop: 1
 };
+function updateObjectWithKeyAndValue(recipes, prop, value) {
+
+}
 function destructivelyUpdateObjectWithKeyAndValue(recipes, prop, value) {
   recipes.prop2 = 2;
   return recipes;
+}
+function deleteFromObjectByKey(recipes, prop) {
+  
 }
 function destructivelyDeleteFromObjectByKey(recipes, prop) {
   delete recipes.prop;
