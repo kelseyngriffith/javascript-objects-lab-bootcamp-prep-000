1917269a6ae4baaa25cb9afac6a60782759a022e
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-07-06 01:33:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {
   prop: 1
 };
 function updateObjectWithKeyAndValue(recipes, prop, value) {
-  recipes.prop2 = '2';
+  recipes.prop2 = 2;
   return recipes;
 }
 updateObjectWithKeyAndValue(recipes);
