d0d5e32778e6b456ca95889395df410f817d7019
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-07-06 01:39:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, prop, value) {
   recipes.prop2 = 2;
   return recipes;
 }
-function deleteFromObjectByKey(recipes, prop) {
+function destructivelyDeleteFromObjectByKey(recipes, prop) {
   delete recipes.prop;
 }
